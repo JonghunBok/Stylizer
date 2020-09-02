13775974dd2a25100473932227bd2f59bd99f519
Jonghun Park <jonghun.park.194@gmail.com>
2020-09-02 15:54:03 +0900

    Test a basic canvas feature

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-container>
+      <VideoCard v-if="myStream != null" :mediaStream="myStream"/>
+    </v-container>
+
+  </v-app>
 </template>
 
+<style>
+  video, canvas {
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld';
+import VideoCard from './components/VideoCard';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    VideoCard,
+  },
+
+  data: () => ({
+    myStream: null
+  }),
+
+  created () {
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false
+    }).then(stream => {
+      this.myStream = stream
+      console.log(this.myStream);
+    })
+  }
+
+
+};
+</script>
---
+++ src/components/VideoCard.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-card
+      class="mx-auto"
+      max-width="360"
+    >
+      <canvas id="myCanvas"></canvas>
+      <v-btn color="success" dark fab>
+        <v-icon>mdi-domain</v-icon>
+      </v-btn>
+
+      <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+      <v-card-text class="text--primary">
+        <div>Whitehaven Beach</div>
+
+        <div>Whitsunday Island, Whitsunday Islands</div>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn
+          color="orange"
+          text
+        >
+          Share
+        </v-btn>
+
+        <v-btn
+          color="orange"
+          text
+        >
+          Explore
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: 'VideoCard',
+
+  props: {
+    mediaStream: MediaStream
+  },
+
+  mounted() {
+    const video = document.createElement('video')
+    //const video = this.$el.querySelector("video")
+    const canvas = this.$el.querySelector("canvas")
+
+    video.srcObject = this.mediaStream
+
+    video.addEventListener('loadedmetadata', () => {
+      video.play()
+    })
+
+    video.addEventListener('play', () => {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      draw(video, canvas.getContext('2d'), video.videoWidth, video.videoHeight);
+    }, false)
+
+    function draw( video, context, width, height) {
+      var image, data, i, r, g, b, brightness;
+
+      context.drawImage(video, 0, 0, width, height);
+      image = context.getImageData(0, 0, width, height);
+      data = image.data;
+
+      for (i = 0; i < data.length; i += 4) {
+        r = data[i];
+        g = data[i + 1];
+        b = data[i + 2];
+        brightness = ( r + g + b ) / 3;
+        // brightness = brightness > 130 ? brightness : 130;
+
+        data[i] = data[i + 1] = data[i + 2] = brightness;
+      }
+      //console.log(image);
+
+      //image.data = data;
+
+      context.putImageData( image, 0, 0);
+      setTimeout(draw, 10, video, context, width, height);
+    }
+
+
+
+  }
+}
+</script>
